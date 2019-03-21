b13db4d3ceb55028c7c426561227cc977541197a
Maxime Salomon <maxime@croissant.io>
2019-03-21 11:14:14 +0100

    dotenv PORT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
